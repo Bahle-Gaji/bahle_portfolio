60fe0e97ddb13454aa3809241661b19eca278d38
Bahle-Gaji <Bahle.gaji01@gmail.com>
2026-07-22 00:11:14 +0200

    Create typed phrases

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed("#text", {
+    strings:["stand out online.", "build credibility.", "attract more customers." 
+    ],
+    typeSpeed:60,
+    backSpeed:60,
+    loop:true,
+})
