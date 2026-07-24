0b49dbf15c01df1a88a29c3cdc86be0cf4929df9
Bahle-Gaji <Bahle.gaji01@gmail.com>
2026-07-25 01:03:45 +0200

    Create sticky nav

--- script.js
+++ script.js
@@ -4,4 +4,16 @@ var typed = new Typed("#text", {
     typeSpeed:60,
     backSpeed:60,
     loop:true,
+});
+
+// In-place Nav
+let nav = document.querySelector("nav");
+
+window.addEventListener('scroll', () => {
+    if(window.scroll > 100){
+        nav.classList.add("sticky_nav");
+    }
+    else{
+        nav.classList.remove("sticky_nav");
+    }
 })
