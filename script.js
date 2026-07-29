85cb0f0813de83bae56a40564dfb13341e35dc0f
Bahle-Gaji <Bahle.gaji01@gmail.com>
2026-07-29 03:40:57 +0200

    Create nav down and toggle

--- script.js
+++ script.js
@@ -1,19 +1,44 @@
 var typed = new Typed("#text", {
-    strings:["stand out online.", "build credibility.", "attract more customers." 
+    strings: ["stand out online.", "build credibility.", "attract more customers."
     ],
-    typeSpeed:60,
-    backSpeed:60,
-    loop:true,
+    typeSpeed: 60,
+    backSpeed: 60,
+    loop: true,
 });
 
 // In-place Nav
 let nav = document.querySelector("nav");
 
 window.addEventListener('scroll', () => {
-    if(window.scrollY > 100){
+    if (window.scrollY > 100) {
         nav.classList.add("sticky_nav");
     }
-    else{
+    else {
         nav.classList.remove("sticky_nav");
     }
+});
+
+const menu = document.querySelector(".menu");
+const bars = document.querySelector(".bars");
+
+bars.addEventListener('click', () => {
+    menu.classList.toggle("active")
+    bars.querySelector("i").classList.toggle("ri-menu-2-line");
+    bars.querySelector("i").classList.toggle("ri-close-line");
+});
+
+
+
+const links = document.querySelectorAll(".menu a");
+
+links.forEach(link => {
+
+    link.addEventListener("click", () => {
+
+        menu.classList.remove("active");
+        bars.querySelector("i").classList.toggle("ri-menu-2-line");
+        bars.querySelector("i").classList.toggle("ri-close-line");
+        
+    });
+
 });
