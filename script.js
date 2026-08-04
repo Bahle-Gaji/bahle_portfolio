5bec6c08221aad5bc517a1b9771dd2d3460d6322
Bahle-Gaji <Bahle.gaji01@gmail.com>
2026-08-04 10:28:05 +0200

    Update fade in code

--- script.js
+++ script.js
@@ -38,7 +38,7 @@ links.forEach(link => {
         menu.classList.remove("active");
         bars.querySelector("i").classList.toggle("ri-close-line");
         bars.querySelector("i").classList.toggle("ri-menu-2-line");
-        
+
     });
 
 });
@@ -49,15 +49,21 @@ const fadedElements = document.querySelectorAll('.fade-in');
 
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
-        if(entry.isIntersecting){
+        if (entry.isIntersecting) {
             entry.target.classList.add('show');
         }
     })
 }, {
     threshold: 0.2
-} 
+}
 );
 
 fadedElements.forEach(item => {
     observer.observe(item);
-})
+});
+
+// const portfolioCards = document.querySelectorAll(".portfolio_img");
+
+// portfolioCards.forEach((card, index) => {
+//     card.style.transitionDelay = `${index * 250}ms`;
+// });
